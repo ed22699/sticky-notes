4884fe25a4effe4bcafe15927c57d549f578883e
Sonny <ed22699@bristol.ac.uk>
2024-05-09 19:39:30 +0100

    Change project structure

--- lua/sticky-notes.lua
+++ lua/sticky-notes.lua
@@ -1,1 +1,12 @@
-print("Hello from plugin")
+local augroup = vim.api.nvim_create_augroup("sticky-notes", { clear = true })
+
+local function main()
+  print("Hello from our plugin")
+end
+
+local function setup()
+  vim.api.nvim_create_autocmd("VimEnter",
+    { group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = main })
+end
+
+return { setup = setup }
